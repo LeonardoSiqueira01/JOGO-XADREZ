551746987c12eb0b9e5bd9e7d88757816119c307
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-01-08 16:49:26 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,7 +23,7 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-	
+
 	private void initialSetup() {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
 		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
