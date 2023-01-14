dfb3e52a6d7beaa720989b08379c9d2d309301d1
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-01-13 23:00:01 -0300

    Implementing possible moves of king

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,28 +17,26 @@ public class Program {
 
 		while (true) {
 			try {
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.println("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces(),possibleMoves);
-			System.out.println();
-			System.out.println("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			}
-			catch(ChessException e) {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.println("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.println("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-			catch(InputMismatchException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
-		 }
+		}
 	}
 }
